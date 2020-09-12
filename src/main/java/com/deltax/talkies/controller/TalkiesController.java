ced5f6ead0c2c3df2b3622058a2d419f9ba47d69
Himanshu Sharma <himanshu.sharma@mediaagility.com>
2020-09-13 00:48:45 +0530

    update

--- src/main/java/com/deltax/talkies/controller/TalkiesController.java
+++ src/main/java/com/deltax/talkies/controller/TalkiesController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 /**
  *
  * @author Himanshu
